07000b3caee52ab39ed2a631e26530179beb536a
David Landry <davland7@gmail.com>
2023-11-21 20:40:09 -0500

    Update currencies.ts

--- netlify/functions/currencies.ts
+++ netlify/functions/currencies.ts
@@ -3,7 +3,8 @@ import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': process.env.ACCESS_CONTROL_ALLOW_ORIGIN as string,
   'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Cache-Control': 'public, max-age=450, immutable'
 }
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
