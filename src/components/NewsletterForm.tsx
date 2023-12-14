f265e1de5be293bc3f825fb0c53436fd935456a1
davland7 <davland7@gmail.com>
2023-12-13 20:13:52 -0500

    Test layout

--- src/components/NewsletterForm.tsx
+++ src/components/NewsletterForm.tsx
@@ -1,58 +1,70 @@
 import type { JSX } from 'preact';
-import { useEffect, useState } from 'preact/hooks';
+import { useState } from 'preact/hooks';
 
 function NewsletterForm() {
   const [error, setError] = useState(false);
-  const [disabled, setDisabled] = useState(true);
 
-  const handleKeyboardEvent = (event: JSX.TargetedEvent<HTMLInputElement>) => {
-    const { target } = event;
-
-    if (target) {
-      const email = (target as HTMLFormElement).value;
-      const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
-      const isValid = emailRegex.test(email);
-      setError(!isValid);
-      setDisabled(!isValid);
-
-      console.log((target as HTMLFormElement).value);
-    }
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+    return emailRegex.test(email);
   };
+
+  const handleSubmit = (event: JSX.TargetedEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const email = form.email.value;
+    const emailIsValid = validateEmail(email);
+
+    if (emailIsValid) {
+      setError(false);
+      form.submit();
+    } else {
+      setError(true);
+    }
+  }
 
   return (
     <form
-      class="bg-white shadow-lg rounded-md p-5 md:p-10 flex flex-col w-11/12 max-w-lg group"
+      class="flex flex-col items-center w-11/12 max-w-lg"
       data-netlify="true"
-      netlify-honeypot
       name="newsletter"
       method="POST"
-      action="/fr/merci"
+      onSubmit={handleSubmit}
       noValidate
     >
-      <p class="hidden">
-        <label>
-          Don’t fill this out if you’re human: <input name="bot-field" />
-        </label>
+      <input
+        type="hidden"
+        name="form-name"
+        value="newsletter"
+      />
+      <p
+        role="alert"
+        aria-live="assertive"
+        class="text-red-500 h-12"
+      >
+        {error && 'Veuillez saisir une adresse e-mail valide.'}
       </p>
-      <input type="hidden" name="form-name" value="newsletter" />
-      <label for="email" class="mb-5">Adresse e-mail</label>
+      <label
+        for="email"
+        class="sr-only"
+      >
+        Adresse e-mail
+      </label>
       <input
         type="email"
         name="email"
         aria-labelledby="email-label"
-        class={`w-full h-12 rounded-full border bg-inherit p-3 shadow shadow-gray-100 mt-2 appearance-none outline-none text-neutral-800 ${error ? 'border-red-500' : 'border-gray-300'}`}
+        class={`w-full h-12 rounded-full border bg-inherit p-3 shadow shadow-gray-100 appearance-none outline-none text-neutral-800 ${error ? 'border-red-500' : 'border-orange-500'}`}
         id="email"
-        onKeyUp={handleKeyboardEvent}
+        placeholder="Adresse e-mail"
         required
       />
-
-      {error && (
-        <p role="alert" aria-live="assertive" class="mt-5 bg-red-100 border border-red-500 text-red-500 text-sm p-3 rounded-full h-12">
-          Veuillez saisir une adresse e-mail valide.
-        </p>
-      )}
-
-      <button class="mt-5 p-3 rounded-full border disabled:bg-orange-400 bg-orange-500 h-12" type="submit" disabled={disabled} aria-label="Submit">
+      <button
+        class="p-3 rounded-full border bg-orange-500 h-12 w-full mt-5 text-white font-semibold hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:ring-offset-white focus:ring-opacity-50"
+        type="submit"
+        aria-label="Submit"
+      >
         Submit
       </button>
     </form>
