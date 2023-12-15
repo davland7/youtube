a9a5b52d3770e038a48e90d3dfab98a226c5f373
davland7 <davland7@gmail.com>
2023-12-15 02:37:43 -0500

    Currency Converter

--- src/components/NewsletterForm.tsx
+++ src/components/NewsletterForm.tsx
@@ -27,7 +27,7 @@ function NewsletterForm() {
       fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "newsletter", email })
+        body: encode({ "form-name": "newsletter", "email": email })
       })
         .then((response) => {
           console.log(response);
