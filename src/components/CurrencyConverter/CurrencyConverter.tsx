9ca428b4205d1043a170b520635868953a8146c3
davland7 <davland7@gmail.com>
2023-12-17 07:24:56 -0500

    Update Astro 4

--- src/components/CurrencyConverter/CurrencyConverter.tsx
+++ src/components/CurrencyConverter/CurrencyConverter.tsx
@@ -23,7 +23,7 @@ const App = () => {
 
   useEffect(() => {
     async function getData() {
-      const response = await fetch('/.netlify/functions/currencies', {
+      const response = await fetch('/api/currencies', {
         method: 'POST',
         signal: controller.signal
       });
