877e20f3bf350d723e0db4b0ad0d3560053702bd
davland7 <davland7@gmail.com>
2023-12-17 08:33:01 -0500

    Update Astro 4

--- netlify/functions/currencies.ts
+++ netlify/functions/currencies.ts
@@ -3,18 +3,11 @@ import type { Handler, HandlerEvent } from "@netlify/functions";
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': process.env.ACCESS_CONTROL_ALLOW_ORIGIN as string,
   'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+  'Access-Control-Allow-Methods': 'GET, POST'
 }
 
 const handler: Handler = async (event: HandlerEvent) => {
   try {
-    if (event.httpMethod === 'OPTIONS') {
-      return {
-        statusCode: 200,
-        headers: CORS_HEADERS,
-      }
-    }
-
     const response = await fetch(process.env.GOOGLE_SHEETS_URL as string);
 
     const csvData = await response.text();
