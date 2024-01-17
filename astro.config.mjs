c66c69804e17f093dc5e7f943be8f42cd38f6b43
davland7 <davland7@gmail.com>
2024-01-17 06:58:41 -0500

    PWA Test Maskable

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,8 @@ export default defineConfig({
           {
             "src": "icons/android-chrome-192x192.png",
             "sizes": "192x192",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "any",
           },
           {
             "src": "icons/android-chrome-maskable-192x192.png",
@@ -29,7 +30,8 @@ export default defineConfig({
           {
             "src": "icons/android-chrome-512x512.png",
             "sizes": "512x512",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "any",
           },
           {
             "src": "icons/android-chrome-maskable-512x512.png",
@@ -39,6 +41,7 @@ export default defineConfig({
           }
         ],
         "lang": "fr",
+        "launcherName": "code • finance",
         "name": "code • finance",
         "orientation": "portrait",
         "short_name": "@davland7",
