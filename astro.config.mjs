a6e2c3d7dd95bf0e5df35ba64b001ddbae385caa
davland7 <davland7@gmail.com>
2024-01-17 04:05:59 -0500

    PWA Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,20 +23,26 @@ export default defineConfig({
         scope: "/",
         icons: [
           {
-            src: "images/pwa-192x192.png",
+            src: "icons/android-chrome-maskable-192x192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "images/pwa-192x192.png",
+            src: "icons/android-chrome-maskable-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable"
           },
           {
-            src: "images/pwa-512x512.png",
+            src: "icons/android-chrome-maskable-512x512.png",
             sizes: "512x512",
             type: "image/png"
+          },
+          {
+            src: "icons/android-chrome-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
           }
         ]
       },
