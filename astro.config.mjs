09bb446f608b9ed41b91889fb2083ed3c7b7441a
davland7 <davland7@gmail.com>
2024-01-17 04:21:59 -0500

    PWA Test Maskable

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,6 +37,12 @@ export default defineConfig({
             src: "icons/android-chrome-maskable-512x512.png",
             sizes: "512x512",
             type: "image/png"
+          },
+          {
+            src: "icons/android-chrome-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
           }
         ]
       },
