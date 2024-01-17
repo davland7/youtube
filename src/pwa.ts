f75a2a7261cbe1c223f6d64374effbafed45e42e
davland7 <davland7@gmail.com>
2024-01-17 07:40:06 -0500

    PWA Test Maskable

--- src/pwa.ts
+++ src/pwa.ts
@@ -3,9 +3,23 @@ import { registerSW } from 'virtual:pwa-register';
 registerSW({
   immediate: true,
   onRegisteredSW(swScriptUrl) {
-    console.log('SW registered: ', swScriptUrl)
+    console.log('SW registered: ', swScriptUrl);
   },
   onOfflineReady() {
-    console.log('PWA application ready to work offline')
+    console.log('PWA application ready to work offline');
+
+    // Exemple de notification
+    showNotification('PWA Prête', 'L\'application est prête à fonctionner hors ligne!');
   },
-})
+});
+
+function showNotification(title: string, message: string) {
+  // Vérifier si le navigateur prend en charge les notifications
+  if ('Notification' in window) {
+    Notification.requestPermission().then((permission) => {
+      if (permission === 'granted') {
+        new Notification(title, { body: message });
+      }
+    });
+  }
+}
