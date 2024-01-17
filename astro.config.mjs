14b510dcead1e3b507439ac088165240020cf43a
davland7 <davland7@gmail.com>
2024-01-16 20:10:53 -0500

    PWA Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,6 +28,12 @@ export default defineConfig({
             type: "image/png"
           },
           {
+            src: "images/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
             src: "images/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png"
