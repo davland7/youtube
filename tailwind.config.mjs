9fc85795be14ce3113f12dd92f62db72b988798e
davland7 <davland7@gmail.com>
2024-01-17 19:36:13 -0500

    PWA Test Maskable

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
-	theme: { },
+	theme: {
+    extend: {
+      colors: {
+        primary: '#e95d2a',
+        secondary: '#f4f4f6',
+      },
+    },
+  },
 	plugins: [
 		require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
